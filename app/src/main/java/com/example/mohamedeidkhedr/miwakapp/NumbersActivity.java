68f0000e39e13ea7c5b2a0d7c459a3d4872a0449
mohamedeidkhedr <mohamedkhedr0@gmail.com>
2018-11-27 02:50:57 +0300

    Add audiofocus Add up button in navigation bar

--- app/src/main/java/com/example/mohamedeidkhedr/miwakapp/NumbersActivity.java
+++ app/src/main/java/com/example/mohamedeidkhedr/miwakapp/NumbersActivity.java
@@ -1,5 +1,6 @@
 package com.example.mohamedeidkhedr.miwakapp;
 
+import android.content.Context;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
@@ -13,18 +14,44 @@ import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
     MediaPlayer mediaPlayer;
+    AudioManager audioManager;
+    AudioManager.OnAudioFocusChangeListener focusChangeListener=new AudioManager.OnAudioFocusChangeListener() {
+        @Override
+        public void onAudioFocusChange(int i) {
+            if (i == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT || i == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK) {
+                mediaPlayer.pause();
+                mediaPlayer.seekTo(0);
+            } else if (i == AudioManager.AUDIOFOCUS_GAIN_TRANSIENT || i == AudioManager.AUDIOFOCUS_GAIN) {
+                mediaPlayer.start();
+            } else if (i == AudioManager.AUDIOFOCUS_LOSS) {
+                releaseMediaplayer(mediaPlayer);
+            }
+
+        }
+    };
+
+
     MediaPlayer.OnCompletionListener onCompletionListener = new MediaPlayer.OnCompletionListener() {
         @Override
         public void onCompletion(MediaPlayer mediaPlayer) {
-            mediaPlayer.release();
+            releaseMediaplayer(mediaPlayer);
         }
     };
 
 
     @Override
+    protected void onStop() {
+        super.onStop();
+        releaseMediaplayer(mediaPlayer);
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+        audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+
+
         final ArrayList<Word> words = new ArrayList<>();
         words.add(new Word(getString(R.string.number_one), getString(R.string.number_one), R.drawable.number_one, R.raw.number_one));
         words.add(new Word(getString(R.string.number_two), getString(R.string.number_two), R.drawable.number_two, R.raw.number_two));
@@ -37,24 +64,43 @@ public class NumbersActivity extends AppCompatActivity {
         words.add(new Word(getString(R.string.number_nine), getString(R.string.number_one), R.drawable.number_nine, R.raw.number_nine));
         words.add(new Word(getString(R.string.number_ten), getString(R.string.number_one), R.drawable.number_ten, R.raw.number_ten));
 
+
 //        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, words);
 //        ListView listView = (ListView) findViewById(R.id.root_view);
 //        listView.setAdapter(adapter);
         WordAdapter adapter = new WordAdapter(this, words, R.color.numberColor);
         ListView listView = (ListView) findViewById(R.id.numbers_list_view);
         listView.setAdapter(adapter);
+
+
         AdapterView.OnItemClickListener onItemClickListener = new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                releaseMediaplayer(mediaPlayer);
                 mediaPlayer = MediaPlayer.create(NumbersActivity.this, words.get(i).getAudioResource());
-                mediaPlayer.start();
-                mediaPlayer.setOnCompletionListener(onCompletionListener);
+                int result = audioManager.requestAudioFocus(focusChangeListener, AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
+                if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+                    mediaPlayer.start();
+                    mediaPlayer.setOnCompletionListener(onCompletionListener);
+                }
+
+
             }
         };
 
 
         listView.setOnItemClickListener(onItemClickListener);
 
+
+    }
+
+    private  void releaseMediaplayer(MediaPlayer mediaPlayer) {
+        if (mediaPlayer != null) {
+            mediaPlayer.release();
+
+        }
+        mediaPlayer = null;
+        audioManager.abandonAudioFocus(focusChangeListener);
 
     }
 }
